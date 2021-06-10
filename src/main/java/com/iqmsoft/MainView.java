f0aff6933dd446d7564a5641ef0897c4e0cd61a2
Muruga <davanon2014@gmail.com>
2021-06-10 11:11:34 +0530

    First

---
+++ src/main/java/com/iqmsoft/MainView.java
@@ -0,0 +1,27 @@
+package com.iqmsoft;
+
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+import javax.inject.Inject;
+
+/**
+ * The main view contains a simple label element and a template element.
+ */
+@Route
+@HtmlImport("css/shared-styles.html")
+public class MainView extends VerticalLayout {
+
+    @Inject
+    private MessageBean messageBean;
+
+    public MainView() {
+        Button button = new Button("Click me",
+                event -> Notification.show(messageBean.getMessage()));
+        add(button);
+    }
+
+}
